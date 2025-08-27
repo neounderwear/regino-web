8d49f86b3932e68e7a43a09a216c1f95db9fd6e1
herlan-jae <herlanjaelani1507@gmail.com>
2025-08-27 21:26:08 +0700

    add env

--- src/Firebase.ts
+++ src/Firebase.ts
@@ -1,15 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDLLvlVCHjhaWZty5PpJoWPdH8c7kBIr5Q",
-  authDomain: "gudangpd-project.firebaseapp.com",
-  projectId: "gudangpd-project",
-  storageBucket: "gudangpd-project.firebasestorage.app",
-  messagingSenderId: "259576155613",
-  appId: "1:259576155613:web:044875f3d74c44c25b3a34",
-  measurementId: "G-RJ929SXFGZ",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
